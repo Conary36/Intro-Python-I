42530d3a85880ee0bb906ebba8bd8d959b807dfa
Conary36 <cbeckford144@gmail.com>
2020-09-02 17:22:16 -0400

    initial commit ..

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,7 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+def to_pow():
+    print(2**65536)
+to_pow()
--- src/04_printing.py
+++ src/04_printing.py
@@ -3,6 +3,10 @@ Python provides a number of ways to perform printing. Research
 how to print using the printf operator, the `format` string 
 method, and by using f-strings.
 """
+import sys
+def printf(format, *args):
+    sys.stdout.write(format, args)
+
 
 x = 10
 y = 2.24552
@@ -11,6 +15,7 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+printf("x=%d, y=%.2f, z=%", x, y, z)
 
 # Use the 'format' string method to print the same thing
 
